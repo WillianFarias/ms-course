9ea0453f9d685b235debeb1ae9cb9b0296afe3d5
WillianFarias <williansousafarias@gmail.com>
2024-10-30 21:28:06 -0300

    Populando role e user

--- hr-user/src/main/java/com/devsuperior/hruser/entity/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entity/User.java
@@ -13,8 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 2240179764684548834L;
